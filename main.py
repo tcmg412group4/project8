aea9dfd1c63d4aafff935af8c67dead6f284dead
Austin John <austinjohn@tamu.edu>
2023-04-11 17:33:27 -0500

    docker-compose fixed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import socket
 import json 
 
 
-r = redis.Redis(host="redis", port=6379, decode_responses=True)
+r = redis.Redis(host="myredis", port=6379, decode_responses=True)
 
 
 app = Flask(__name__)
